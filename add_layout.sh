5487ca2ef65d7fe1dadccd909251c87d119a175f
Berk Karaal <karaalberk7@gmail.com>
2022-04-09 15:28:53 +0300

    allow echo to take escape sequences
    In order to display text in multiple lines at the end of the script.

--- add_layout.sh
+++ add_layout.sh
@@ -11,4 +11,4 @@ cp ./X11/xkb/rules/* /usr/share/X11/xkb/rules/
 echo "run: cp ./X11/xkb/symbols/* /usr/share/X11/xkb/symbols/"
 cp ./X11/xkb/symbols/* /usr/share/X11/xkb/symbols/
 
-echo "Done.\nYou may need to logout and login again or restart your computer to take effect."
+echo -e "Done.\nYou may need to logout and login again or restart your computer to take effect."
